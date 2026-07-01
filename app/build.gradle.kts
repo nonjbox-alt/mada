3f26407d98205854c3a30edfc1e031986aaf1281
nonjbox <nonjbox@gmail.com>
2026-07-01 20:28:47 +0300

    Fix Compose plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
-
 android {
     namespace  = "com.mada"
     compileSdk = 34
@@ -21,21 +21,12 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
     kotlinOptions {
         jvmTarget = "17"
     }
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packaging {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,12 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("com.android.application") version "8.5.2"
+        id("org.jetbrains.kotlin.android") version "2.0.20"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    }
 }
 
 dependencyResolutionManagement {
